eecab6e2645b16aca17dd6b7dac630f5d10f2a25
pluto476526 <ngugikibuka99@gmail.com>
2024-10-20 10:30:44 +0300

    Auto commit

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+conn = mysqli.connector.connect(
+        host = "localhost",
+        user = "trinity985",
+        password = "37022657",
+        database = "alx_book_store"
+)
+
+def createDB():
+    miCursor = conn.cursor()
+    sql = "CREATE DATABASE IF NOT EXIST alx_book_store"
+
+    if miCursor.execute(sql) :
+        print("Database 'alx_book_store' created succesfully!")
+
+    conn.commit()
