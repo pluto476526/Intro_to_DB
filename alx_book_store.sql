9528f792018cfa793457ea1bee9583f7eb27ac54
pluto476526 <ngugikibuka99@gmail.com>
2024-10-20 10:17:12 +0300

    Auto commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS Authors (author_id INT AUTO_INCREMENT PRIMARY KEY, au
 
 CREATE TABLE IF NOT EXISTS Customers (customer_id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
 
-CREATE TABLE IF NOT EXISTS Orders (order_id INT AUTO_INCREMENT PRIMARY KEY, customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers (customer_id));
+CREATE TABLE IF NOT EXISTS Orders (order_id INT AUTO_INCREMENT PRIMARY KEY, customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id));
 
-CREATE TABLE IF NOT EXISTS Order_Details (order_detail_id INT AUTO_INCREMENT PRIMARY KEY, order_id INT, book_id INT, quantity DOUBLE, FOREIGN KEY (order_id) REFERENCES Orders (order_id), FOREIGN KEY (book_id) REFERENCES Books (book_id));
+CREATE TABLE IF NOT EXISTS Order_Details (order_detail_id INT AUTO_INCREMENT PRIMARY KEY, order_id INT, book_id INT, quantity DOUBLE, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id));
